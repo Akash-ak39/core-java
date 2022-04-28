ae105c68a8d2d8549f5c4ecb9bda6c12773024ae
akash.sureshbabu <akash.sureshbabu@DT150139.QICHOAPPL.com>
2022-04-28 09:47:44 +0530

    checking

---
+++ Basicjava/src/main/java/Push_Pull_sonar.java
@@ -0,0 +1,14 @@
+
+public class Push_Pull_sonar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='i';
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u');
+		System.out.println(ch+"is vowel");
+		System.out.println(ch+"is consonant");
+		
+
+	}
+
+}
