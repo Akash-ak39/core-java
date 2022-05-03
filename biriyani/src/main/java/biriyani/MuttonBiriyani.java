544ad3239df6ce11f49811e8c0a12125412e20bc
akash.sureshbabu <akash.sureshbabu@DT150139.QICHOAPPL.com>
2022-05-03 11:35:57 +0530

    where is biriyani...

--- biriyani/src/main/java/biriyani/MuttonBiriyani.java
+++ biriyani/src/main/java/biriyani/MuttonBiriyani.java
@@ -1,23 +1,21 @@
-package sonar;
+package biriyani;
 
 import java.io.File;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
-public class Push_pull_sonar {
-	static Logger logger=Logger.getLogger(Push_pull_sonar.class);
+public class MuttonBiriyani {
+	static Logger logger=Logger.getLogger(MuttonBiriyani.class);
 
 	public static void main(String[] args) {
-String str="welcome to sonar";
 		
+		String str="Wee all love Biriyani";
 		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
 		PropertyConfigurator.configure(log4jConfigFile);
-		logger.info("this is a message....:"+str);
-		logger.warn("warning message...:"+str);
-		logger.error("error message...:"+str);
-		
-		//yes its worked
+		logger.info("cost of biriyani is Rupees 120");
+		logger.warn("biriyani makes you obesee");
+		logger.error("emptied");
 		
 
 	}
