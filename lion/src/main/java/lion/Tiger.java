d6dc19873556d82cdba6b03be81f33c008a1bed0
Akash.S <104414492+Akash-ak39@users.noreply.github.com>
2022-05-05 14:16:05 +0530

    yess

--- lion/src/main/java/lion/Tiger.java
+++ lion/src/main/java/lion/Tiger.java
@@ -5,6 +5,8 @@ public class Tiger {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("bncvm ");
+		String str="lion is king of the forest";
+		
 	}
 
 }
